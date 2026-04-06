252d0decb41c89ef94da95d45272dc96a67b05d2
rayzanbran <218662019+rayzanbran@users.noreply.github.com>
2026-04-06 15:04:55 -0400

    said hello to world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    @Override
+    public String toString() {
+        return "Hello World";
+    }
+    public static void main(String[] args) {
+        HelloWorld helloworld = new HelloWorld();
+        System.out.println(helloworld);
+    }
+}
+
